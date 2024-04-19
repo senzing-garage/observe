5b21ace7f1a493fdf065f3641ae02c8a54f48981
docktermj <michael@dockter.com>
2024-04-19 15:03:41 -0400

    Fix generated file

--- cmd/github.go
+++ cmd/github.go
@@ -1,18 +1,3 @@
-// 0.1.5
-// Created by make-go-github-file.yaml on Mon Oct 23 13:32:53 UTC 2023
-//
-//lint:file-ignore U1000 Ignore all unused code, it's generated
-package cmd
-
-var (
-	githubDate           string = "2024-01-02"
-	githubIteration      string = "0"
-	githubRef            string = "refs/tags/0.2.0"
-	githubRefName        string = "0.2.0"
-	githubRepository     string = "Senzing/observe"
-	githubRepositoryName string = "observe"
-	githubVersion        string = "0.2.0"
-)
 // 0.2.2
 // Created by make-go-github-file.yaml on Fri Apr 19 18:56:44 UTC 2024
 //
