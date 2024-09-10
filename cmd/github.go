5353b34129d846cc95fbff8214eefe899efaa61d
senzingdevops <senzingdevops@users.noreply.github.com>
2024-09-10 00:12:46 +0000

    make-go-github-file.yaml updated cmd/github.go for versioned release: 0.3.2

--- cmd/github.go
+++ cmd/github.go
@@ -1,15 +1,15 @@
-// 0.3.1
-// Created by senzing-factory/github-action-make-go-github-file.yaml on Wed Aug 21 17:42:32 UTC 2024
+// 0.3.2
+// Created by senzing-factory/github-action-make-go-github-file.yaml on Tue Sep 10 00:12:46 UTC 2024
 //
 //lint:file-ignore U1000 Ignore all unused code, it's generated
 package cmd
 
 var (
-	githubDate           = "2024-08-21"
+	githubDate           = "2024-09-10"
 	githubIteration      = "0"
-	githubRef            = "refs/tags/0.3.1"
-	githubRefName        = "0.3.1"
+	githubRef            = "refs/tags/0.3.2"
+	githubRefName        = "0.3.2"
 	githubRepository     = "senzing-garage/observe"
 	githubRepositoryName = "observe"
-	githubVersion        = "0.3.1"
+	githubVersion        = "0.3.2"
 )
